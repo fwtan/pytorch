f52e48bcf3f96781e5ae0ec2a50d30c266534c66
Animesh Jain <anijain@umich.edu>
2025-01-03 18:59:21 -0800

    [dynamo][user-defined] Remove __getattribute__ checks
    ghstack-source-id: 796db174003a8b423e6676172b72f7f44f1ce065
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144173

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -936,10 +936,6 @@ class UserDefinedObjectVariable(UserDefinedVariable):
 
         return super().call_function(tx, args, kwargs)
 
-    def _check_for_getattribute(self):
-        if object_has_getattribute(self.value):
-            unimplemented("UserDefinedObjectVariable with custom __getattribute__")
-
     def _check_for_getattr(self):
         return get_custom_getattr(self.value)
 
@@ -978,7 +974,6 @@ class UserDefinedObjectVariable(UserDefinedVariable):
         return subobj
 
     def has_key_in_generic_dict(self, tx: "InstructionTranslator", key):
-        self._check_for_getattribute()
         if tx.output.side_effects.has_pending_mutation_of_attr(self, key):
             mutated_attr = tx.output.side_effects.load_attr(self, key, deleted_ok=True)
             return not isinstance(mutated_attr, variables.DeletedVariable)
